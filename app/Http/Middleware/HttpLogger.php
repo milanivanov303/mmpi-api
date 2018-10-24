23d902748b6d9f1c2ae0a5e77a0527c6d515e403
Yordan Arnaudov <yarnaudov@codixfr.bg>
2018-10-24 15:57:36 +0300

    Rework http logger to save ready curl requests

--- app/Http/Middleware/HttpLogger.php
+++ app/Http/Middleware/HttpLogger.php
@@ -18,10 +18,12 @@ class HttpLogger
      */
     public function handle(Request $request, Closure $next)
     {
+        $response = $next($request);
+
         if (env('APP_DEBUG', true)) {
-            $this->log($request);
+            $this->log($request, $response);
         }
-        return $next($request);
+        return $response;
     }
     
     /**
@@ -30,9 +32,40 @@ class HttpLogger
      * @param Request $request
      * @param Response $response
      */
-    protected function log(Request $request)
+    protected function log(Request $request, Response $response)
     {
-        // generate curl command here, so it can be used directly!
-        Log::channel('http')->info($request->__toString());
+        Log::channel('http')->info(
+            PHP_EOL .
+            $this->getRequestCurl($request) .
+            PHP_EOL .
+            PHP_EOL .
+            $response->getStatusCode() . ' ' . $response->getContent().
+            PHP_EOL
+        );
+    }
+
+    /**
+     * Get request cURL command
+     *
+     * @param Request $request
+     * @return string
+     */
+    protected function getRequestCurl(Request $request)
+    {
+        $curl = [];
+
+        $curl[] = "curl -X {$request->getMethod()}";
+        $curl[] = "'{$request->getUri()}'";
+
+        foreach ($request->header() as $header => $value) {
+            $curl[] = "-H '{$header}: {$value[0]}'";
+        }
+
+        if ($request->json()->all()) {
+            $data = json_encode($request->json()->all(), JSON_PRETTY_PRINT);
+            $curl[] = "-d '{$data}'";
+        }
+
+        return implode(" \\" . PHP_EOL, $curl);
     }
 }
