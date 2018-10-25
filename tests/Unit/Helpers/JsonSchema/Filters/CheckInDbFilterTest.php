f1857b32954c4b796ab9855f38c3ff6c73b11607
Yordan Arnaudov <yarnaudov@codix.bg>
2018-10-25 14:57:55 +0300

    Add full coverage to CheckInDbFilter unit tests

--- tests/Unit/Helpers/JsonSchema/Filters/CheckInDbFilterTest.php
+++ tests/Unit/Helpers/JsonSchema/Filters/CheckInDbFilterTest.php
@@ -14,11 +14,20 @@ class CheckInDbFilterTest extends TestCase
         parent::setUp();
 
         $this->filter = new CheckInDbFilter();
+
+        // Mock Request
+        app()->bind(\Illuminate\Http\Request::class, function () {
+            $mock = \Mockery::mock(\Illuminate\Http\Request::class)->makePartial();
+            $mock->shouldReceive('method')->once()->andReturn('PUT');
+            return $mock;
+        });
     }
 
     public function test_calls_validator()
     {
-        $name = 'jdoe';
+        $faker = Faker\Factory::create();
+
+        $name = $faker->userName;
         $rule = 'exists:users,username';
 
         // Mock Validator
@@ -30,21 +39,29 @@ class CheckInDbFilterTest extends TestCase
         $this->filter->validate($name, ['rule' => $rule]);
     }
 
+    public function test_does_not_calls_validator_fot_nulls()
+    {
+        $name = null;
+        $rule = 'exists:users,username';
+
+        // Mock Validator
+        Validator::shouldReceive('make')->never();
+
+        $result = $this->filter->validate($name, ['rule' => $rule]);
+
+        $this->assertTrue($result);
+    }
+
     public function test_adds_id_on_update_unique()
     {
-        $name = 'jdoe';
+        $faker = Faker\Factory::create();
+
+        $name = $faker->userName;
         $rule = 'unique:users,username';
 
         // create new model and manually set its id
         $model = new Model();
         $model->id = 12;
-
-        // Mock Request
-        app()->bind(\Illuminate\Http\Request::class, function () {
-            $mock = \Mockery::mock(\Illuminate\Http\Request::class)->makePartial();
-            $mock->shouldReceive('method')->once()->andReturn('PUT');
-            return $mock;
-        });
 
         // Mock DB
         DB::shouldReceive('table')
@@ -55,6 +72,27 @@ class CheckInDbFilterTest extends TestCase
         Validator::shouldReceive('make')
             ->once()
             ->with(['field' => $name], ['field' =>$rule . ',' . $model->id])
+            ->andReturn(Mockery::mock(['passes' => true]));
+
+        $this->filter->validate($name, ['rule' => $rule]);
+    }
+
+    public function test_does_not_adds_id_on_update_unique_when_no_model_found()
+    {
+        $faker = Faker\Factory::create();
+
+        $name = $faker->userName;
+        $rule = 'unique:users,username';
+
+        // Mock DB
+        DB::shouldReceive('table')
+            ->once()
+            ->andReturn(Mockery::mock(['where' => Mockery::mock(['first' => false])]));
+
+        // Mock Validator
+        Validator::shouldReceive('make')
+            ->once()
+            ->with(['field' => $name], ['field' =>$rule])
             ->andReturn(Mockery::mock(['passes' => true]));
 
         $this->filter->validate($name, ['rule' => $rule]);
