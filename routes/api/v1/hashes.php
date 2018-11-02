ab322561cfb99c45515da48334fdff43c63d8ffe
Yordan Arnaudov <yarnaudov@codix.bg>
2018-11-02 11:03:35 +0200

    Move json shemas to directories

--- routes/api/v1/hashes.php
+++ routes/api/v1/hashes.php
@@ -6,26 +6,26 @@ $router->group([
 ], function () use ($router) {
     $router->get('', [
         'as'          => 'hashes.list',
-        'schema'      => '/api/v1/hash.json',
+        'schema'      => '/api/v1/hashes/hash.json',
         'description' => 'Get hashes list',
         'uses'        => 'HashesController@getMany'
     ]);
     $router->get('/{rev:[0-9a-z]+}', [
         'as'          => 'hashes.one',
-        'schema'      => '/api/v1/hash.json',
+        'schema'      => '/api/v1/hashes/hash.json',
         'description' => 'Get single hash',
         'uses'        => 'HashesController@getOne'
     ]);
     $router->post('', [
             'as'          => 'hashes.create',
-            'schema'      => '/api/v1/hash.json',
+            'schema'      => '/api/v1/hashes/hash.json',
             'description' => 'Create new hash',
             'uses'        => 'HashesController@create'
     ]);
     $router->put('/{rev:[0-9a-z]+}', [
             'as'          => 'hashes.update',
             'description' => 'Update hash',
-            'schema'      => '/api/v1/hash.json',
+            'schema'      => '/api/v1/hashes/hash.json',
             'uses'        => 'HashesController@update'
     ]);
     $router->delete('/{rev:[0-9a-z]+}', [
--- routes/api/v1/patch-requests.php
+++ routes/api/v1/patch-requests.php
@@ -6,26 +6,26 @@ $router->group([
 ], function () use ($router) {
     $router->get('', [
         'as'          => 'patch-requests.list',
-        'schema'      => '/api/v1/patch-request.json',
+        'schema'      => '/api/v1/patch-requests/patch-request.json',
         'description' => 'Get patch requests list',
         'uses'        => 'PatchRequestsController@getMany'
     ]);
     $router->get('/{tts_id}', [
         'as'          => 'patch-requests.one',
-        'schema'      => '/api/v1/patch-request.json',
+        'schema'      => '/api/v1/patch-requests/patch-request.json',
         'description' => 'Get single patch request',
         'uses'        => 'PatchRequestsController@getOne'
     ]);
     $router->post('', [
         'as'          => 'patch-requests.create',
-        'schema'      => '/api/v1/patch-request.json',
+        'schema'      => '/api/v1/patch-requests/patch-request.json',
         'description' => 'Create new patch request',
         'uses'        => 'PatchRequestsController@create'
     ]);
     $router->put('/{tts_id:[A-Z-0-9]+}', [
         'as'          => 'patch-requests.update',
         'description' => 'Update patch request',
-        'schema'      => '/api/v1/patch-request.json',
+        'schema'      => '/api/v1/patch-requests/patch-request.json',
         'uses'        => 'PatchRequestsController@update'
     ]);
     $router->delete('/{tts_id:[A-Z-0-9]+}', [
