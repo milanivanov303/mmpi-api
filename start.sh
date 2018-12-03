5c98c00285d3aa3ebc5d5183622367a960d2bcb6
Yordan Arnaudov <yarnaudov@codixfr.bg>
2018-12-03 15:44:12 +0200

    Add start script and Docker environment variables in .env

---
+++ start.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# load configuration
+. ./.env
+
+if [ "$1" == "--help" ]
+then
+    echo "Start the application using Docker
+
+SCRIPT PARAMETERS
+
+    N/A
+
+ENV CONFIGS
+
+NGINX config dynamically loaded from ./docker/ndinx/default.conf
+PHP config dynamically loaded from   ./docker/php/php.ini
+MySQL config dynamically loaded from ./docker/mysql/my.cnf
+
+"
+    exit
+fi
+
+if [ -f docker-compose.yml ]
+then
+    echo "Stopping already running containers"
+    docker-compose down
+
+    printf "\n"
+
+    echo "Starting containers ..."
+    docker-compose up --build -d
+    EXIT_CODE=$?
+
+    printf "\n"
+
+    if [ $EXIT_CODE == 0 ]; then
+        printf "\n"
+        echo "App running on port: $PORT"
+    fi
+fi
+
+
