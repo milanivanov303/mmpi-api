115149afa2e5801d19c211a7f59bd3b348b60b62
Yordan Arnaudov <yarnaudov@codix.bg>
2019-05-13 18:29:46 +0300

    Upgrade codix/api-core to 1.3.7 and update scripts

--- scripts/phpcs.sh
+++ scripts/phpcs.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+ARGS="$@"
 
 # load configuration
 . ./.env
 
-docker exec -i -u enterprise ${WEB_CONTAINER_NAME} bash -c ". /enterprise/.profile >/dev/null 2>&1 && ./vendor/bin/phpcs $@"
+docker exec -i -u enterprise ${WEB_CONTAINER_NAME} bash -c ". /enterprise/.profile >/dev/null 2>&1 && ./vendor/bin/phpcs ${ARGS}"
