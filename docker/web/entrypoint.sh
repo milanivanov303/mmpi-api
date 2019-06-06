a015c46f7214799ce8b75c3ad5dfcea23ca01bd9
Yordan Arnaudov <yarnaudov@codix.bg>
2019-06-06 15:42:49 +0300

    Refactor docker settings

--- docker/web/entrypoint.sh
+++ docker/web/entrypoint.sh
@@ -9,14 +9,14 @@ if [ ${ROLE} = "scheduler" ]; then
     echo "Running the scheduler..."
     while [ true ]
     do
-        php /var/www/html/artisan schedule:run --verbose --no-interaction &
+        cd /var/www/html && php artisan schedule:run --verbose --no-interaction &
         sleep 60
     done
 
 elif [ ${ROLE} = "queue" ]; then
 
     echo "Running the queue..."
-    php /var/www/html/artisan queue:work --verbose --tries=3 --timeout=90
+    cd /var/www/html && php artisan queue:work --verbose --tries=3 --timeout=90
 
 else
 
