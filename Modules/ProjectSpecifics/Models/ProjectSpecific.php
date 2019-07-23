ea652aa25ba9a94ca89db99d224ff12c13970d87
Milan Rosenov Ivanov <mrivanov@codixfr.private>
2019-07-23 15:23:05 +0300

    Remove property hidden from class ProjectSpecific

--- Modules/ProjectSpecifics/Models/ProjectSpecific.php
+++ Modules/ProjectSpecifics/Models/ProjectSpecific.php
@@ -10,23 +10,13 @@ use Modules\Projects\Models\Project;
 class ProjectSpecific extends Model
 {
     /**
-     * The attributes that will be hidden in output json
-     *
-     * @var array
-     */
-    protected $hidden = [
-        'prj_specific_feature_id',
-        'project_id',
-        'made_by'
-    ];
-
-    /**
      * The attributes that are mass assignable.
      *
      * @var array
      */
     protected $fillable = [
         'prj_specific_feature_id',
+        'project_id',
         'value',
         'date_characteristic',
         'made_by',
