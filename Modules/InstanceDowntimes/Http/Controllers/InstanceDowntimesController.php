52ed30dc4339a3cb1ec4d81b47ec2ff310383e26
tsdimitrov <tsdimitrov@codix.bg>
2020-06-18 16:45:15 +0300

    Add instance downtinme update mail sending

--- Modules/InstanceDowntimes/Http/Controllers/InstanceDowntimesController.php
+++ Modules/InstanceDowntimes/Http/Controllers/InstanceDowntimesController.php
@@ -4,6 +4,9 @@ namespace Modules\InstanceDowntimes\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Modules\InstanceDowntimes\Repositories\InstanceDowntimeRepository;
+use Illuminate\Http\Request;
+use Modules\InstanceDowntimes\Models\InstanceDowntime;
+use Modules\InstanceDowntimes\Services\NotificationService;
 
 class InstanceDowntimesController extends Controller
 {
@@ -16,5 +19,30 @@ class InstanceDowntimesController extends Controller
     public function __construct(InstanceDowntimeRepository $repository)
     {
         $this->repository = $repository;
+    }
+
+   /**
+     * Update unix access request
+     *
+     * @param Request $request
+     * @param mixed ...$id
+     * @return Response
+     */
+    public function update(Request $request, ...$id)
+    {
+        $data  = $request->all();
+        $model = InstanceDowntime::with(['instance.deliveryChains.projects'])
+            ->findOrFail((int)$id[0]);
+
+        if ($model->count() === 0) {
+            return;
+        }
+
+        app(NotificationService::class, [
+            "model" => $model,
+            "data" => $data
+        ])->sendNotification();
+
+        return parent::update($request, ...$id);
     }
 }
