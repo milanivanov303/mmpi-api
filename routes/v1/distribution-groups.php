dc3473ba9c39ed6e4a467fb79a366a4b71745472
Dimitar Stoynev <dstoynev@codix.eu>
2020-08-19 17:31:16 +0300

    Fix path for schema v2

--- routes/v1/distribution-groups.php
+++ routes/v1/distribution-groups.php
@@ -6,13 +6,13 @@ $router->group([
 ], function () use ($router) {
     $router->get('', [
         'as'          => 'distribution_groups.list',
-        'schema'      => '/v1/distribution-group.json',
+        'schema'      => '/v1/distribution-groups/distribution-group.json',
         'description' => 'Get all distribution groups',
         'uses'        => 'DistributionGroupController@getMany'
     ]);
     $router->get('/{samaccountname}', [
         'as'          => 'distribution_group.one',
-        'schema'      => '/v1/distribution-group.json',
+        'schema'      => '/v1/distribution-groups/distribution-group.json',
         'description' => 'Get single distribution group',
         'uses'        => 'DistributionGroupController@getOne'
     ]);
