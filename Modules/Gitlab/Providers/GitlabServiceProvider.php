219ce212051091002b86ee3eaee50b0d592dcca7
Milan Rosenov Ivanov <mrivanov@codix.eu>
2021-03-15 11:06:58 +0200

    feature/PRT-1531 Add classes in GitlabServiceProvider

--- Modules/Gitlab/Providers/GitlabServiceProvider.php
+++ Modules/Gitlab/Providers/GitlabServiceProvider.php
@@ -3,19 +3,21 @@
 namespace Modules\Gitlab\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use GuzzleHttp\Client as GuzzleHttpClient;
+use Gitlab\Client as GitlabClient;
 
 class GitlabServiceProvider extends ServiceProvider
 {
     public function register()
     {
         $this->app->bind('GitlabApi', function () {
-            $httpClient = new \GuzzleHttp\Client([
+            $httpClient = new GuzzleHttpClient([
                 'verify' => false
             ]);
             
-            $client = \Gitlab\Client::createWithHttpClient($httpClient);
+            $client = GitlabClient::createWithHttpClient($httpClient);
             $client->setUrl(config('app.gitlab.url'));
-            $client->authenticate(config('app.gitlab.token'), \Gitlab\Client::AUTH_HTTP_TOKEN);
+            $client->authenticate(config('app.gitlab.token'), GitlabClient::AUTH_HTTP_TOKEN);
             
             return $client;
         });
