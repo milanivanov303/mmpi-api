c8ec1c2d848eecbcb0fb6fb4fe521c51ed921e7a
Yordan Arnaudov <yarnaudov@codix.bg>
2021-05-05 09:57:57 +0300

    Add audit command

--- App/Console/Kernel.php
+++ App/Console/Kernel.php
@@ -3,6 +3,7 @@
 namespace App\Console;
 
 use App\Console\Commands\HeadMergeCommand;
+use Core\Console\Commands\AuditCommand;
 use Core\Console\Commands\Openapi\Generate as OpenapiGenerate;
 use Illuminate\Console\Scheduling\Schedule;
 use Laravel\Lumen\Console\Kernel as ConsoleKernel;
@@ -28,7 +29,8 @@ class Kernel extends ConsoleKernel
         Tnsnameora::class,
         HashesSynchronizeCommand::class,
         ProjectEventsArchive::class,
-        MissingDeliverychainSourcesCommand::class
+        MissingDeliverychainSourcesCommand::class,
+        AuditCommand::class
     ];
 
     /**
