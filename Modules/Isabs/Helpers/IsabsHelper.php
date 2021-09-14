81824d42eeb0e96dd7fedfd0cc7867d7989a9a16
Milan Ivanov <mrivanov@codix.bg>
2021-09-14 10:04:40 +0300

    New gitlab token & improvement of isabs files

--- Modules/Isabs/Helpers/IsabsHelper.php
+++ Modules/Isabs/Helpers/IsabsHelper.php
@@ -2,6 +2,7 @@
 
 namespace Modules\Isabs\Helpers;
 
+use GuzzleHttp\Client;
 use GuzzleHttp\Exception\GuzzleException;
 use Illuminate\Http\Response;
 
@@ -15,11 +16,73 @@ class IsabsHelper
     protected $url;
 
     /**
+     * Username
+     *
+     * @var string
+     */
+    protected $username;
+
+
+    /**
+     * Password
+     *
+     * @var string
+     */
+    protected $password;
+
+    /**
      * Isabs Helper constructor.
      */
     public function __construct()
     {
         $this->url = config('app.isabs.url');
+        $this->username = config('app.isabs.username');
+        $this->password = config('app.isabs.password');
+    }
+
+    /**
+     * @param string $uri
+     * @return string
+     */
+    protected function getUrl(string $uri) : string
+    {
+        return "{$this->url}/{$uri}";
+    }
+
+    /**
+     * Send request
+     *
+     * @param string $method
+     * @param string $url
+     * @param array $options
+     * @return Response
+     */
+    protected function send(string $method, string $url, array $options) : Response
+    {
+        try {
+            $client = new Client();
+            $response = $client->request($method, $url, $options);
+
+            return Response::create(
+                (string) $response->getBody(),
+                $response->getStatusCode(),
+                [
+                    'Content-Type' => 'application/json'
+                ]
+            );
+        } catch (GuzzleException $e) {
+            if ($e->getResponse()) {
+                return Response::create(
+                    (string) $e->getResponse()->getBody(),
+                    $e->getResponse()->getStatusCode(),
+                    [
+                        'Content-Type' => 'application/json'
+                    ]
+                );
+            }
+
+            return Response::create('Bad Request', 400);
+        }
     }
 
     /**
@@ -29,78 +92,34 @@ class IsabsHelper
      */
     public function login() : Response
     {
-        try {
-            $client = new \GuzzleHttp\Client();
+        $response = $this->send('POST', $this->getUrl('login'), [
+            'verify'  => false,
+            'headers' => [
+                'Content-Type' => 'application/json'
+            ],
+            'body'    => json_encode([
+                "login"    => $this->username,
+                "password" => $this->password
+            ])
+        ]);
 
-            $options = [
-                'verify' => false,
-                'headers' => [
-                    'Content-Type' => 'application/json'
-                ],
-                'body'    => json_encode([
-                    "login" => config('app.isabs.username'),
-                    "password" => config('app.isabs.password')
-                ])
-            ];
+        $content = json_decode($response->content());
 
-            $response = $client->request('POST', $this->url . '/login', $options);
-
-            return Response::create(
-                (string) $response->getBody(),
-                $response->getStatusCode(),
-                [
-                    'Content-Type' => 'application/json'
-                ]
-            );
-        } catch (GuzzleException $e) {
-            if ($e->getResponse()) {
-                return Response::create(
-                    (string) $e->getResponse()->getBody(),
-                    $e->getResponse()->getStatusCode(),
-                    [
-                        'Content-Type' => 'application/json'
-                    ]
-                );
-            }
-
-            return Response::create('Bad Request', 400);
+        if ($response->getStatusCode() === 200 && is_object($content) && property_exists($content, 'token')) {
+            return $response;
         }
+
+        return Response::create('Could not get token', 424);
     }
 
     public function specifications($token)
     {
-        try {
-            $client = new \GuzzleHttp\Client();
-
-            $options = [
-                'verify' => false,
-                'headers' => [
-                    'Content-Type' => 'application/json',
-                    'Authorization' => 'Bearer '. $token,
-                ]
-            ];
-
-            $response = $client->request('GET', $this->url . '/list-tech-names', $options);
-
-            return Response::create(
-                (string) $response->getBody(),
-                $response->getStatusCode(),
-                [
-                    'Content-Type' => 'application/json'
-                ]
-            );
-        } catch (GuzzleException $e) {
-            if ($e->getResponse()) {
-                return Response::create(
-                    (string) $e->getResponse()->getBody(),
-                    $e->getResponse()->getStatusCode(),
-                    [
-                        'Content-Type' => 'application/json'
-                    ]
-                );
-            }
-
-            return Response::create('Bad Request', 400);
-        }
+        return $this->send('GET', $this->getUrl('list-tech-names'), [
+            'verify'  => false,
+            'headers' => [
+                'Content-Type' => 'application/json',
+                'Authorization' => 'Bearer '. $token
+            ]
+        ]);
     }
 }
--- Modules/Isabs/Http/Controllers/IsabsController.php
+++ Modules/Isabs/Http/Controllers/IsabsController.php
@@ -9,47 +9,41 @@ use Illuminate\Support\Facades\Cache;
 
 class IsabsController extends BaseController
 {
-    public function login()
+    public function login(IsabsHelper $helper) : Response
     {
-        $request = new IsabsHelper();
-        $response = $request->login();
-        $content = json_decode($response->content());
-
-        if ($response->getStatusCode() === 200 && is_object($content) && property_exists($content, 'token')) {
-            return $content->token;
-        }
-
-        return ['errors' => $response->getStatusCode() . $response->content()];
+        return $helper->login();
     }
 
     /**
      * Get specs from Isabs
      *
+     * @param IsabsHelper $helper
      * @return Response
      */
-    public function getSpecs() : Response
+    public function getSpecs(IsabsHelper $helper) : Response
     {
-        $token = Cache::has('isabs.token') ? Cache::get('isabs.token') : $this->login();
+        $token = Cache::has('isabs.token') ? Cache::get('isabs.token') : $this->login($helper);
 
-        if (!is_string($token)) {
-            return Response::create('Could not get token', 424);
+        if (is_object($token) && $token->getStatusCode() !== 200) {
+            return $token;
         }
+
+        $token = is_object($token) ? json_decode($token->content())->token : $token;
 
         if (!Cache::has('isabs.token')) {
             //keep token in cache for 8 hours
             Cache::put('isabs.token', $token, 28800);
         }
 
-        $helper = new IsabsHelper();
         $specs = $helper->specifications($token);
 
         if ($specs->getStatusCode() === 401) {
-            $token = $this->login();
-            if (is_string($token)) {
-                Cache::put('isabs.token', $token, 28800);
-                return $helper->specifications($token);
+            $token = $this->login($helper);
+            if (is_object($token) && $token->getStatusCode() === 200) {
+                Cache::put('isabs.token', json_decode($token->content())->token, 28800);
+                return $helper->specifications(json_decode($token->content())->token);
             }
-            return Response::create('Could not get token', 424);
+            return $token;
         }
 
         return $specs;
