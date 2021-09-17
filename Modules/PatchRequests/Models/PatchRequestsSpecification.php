c9d1a21af455dd83cb01eb78de9ba66eb60e45cb
slozanov <slozanov@codix.bg>
2021-09-17 14:45:45 +0300

    revert relation for specificiations and add new model for specifications

---
+++ Modules/PatchRequests/Models/PatchRequestsSpecification.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Modules\PatchRequests\Models;
+
+use Core\Models\Model;
+use App\Models\User;
+use Modules\PatchRequests\Models\PatchRequest;
+
+class PatchRequestsSpecification extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'patch_request_id',
+        'user_id',
+        'specification',
+        'made_on',
+        'made_by'
+    ];
+
+    /**
+     * Get patch request
+     */
+    protected function patchRequests()
+    {
+        return $this->belongsTo(PatchRequest::class, 'id');
+    }
+
+    /**
+     * Get user
+     */
+    protected function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+}
