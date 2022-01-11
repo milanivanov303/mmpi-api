38cb440f4da078ba5d276d43e166bc4fd4ca7c91
Milan Ivanov <mrivanov@codix.bg>
2022-01-11 14:13:40 +0200

    Add status history resource to PatchRequest model

---
+++ Modules/PatchRequests/Models/PatchRequestsStatusHistory.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Modules\PatchRequests\Models;
+
+use Core\Models\Model;
+
+class PatchRequestsStatusHistory extends Model
+{
+    protected $table = 'patch_requests_status_history';
+}
