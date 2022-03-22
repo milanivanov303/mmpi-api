c7973d97adfecd0b1dc7f675a84e96f776299bda
Tsvetomir Dimitrov <tsdimitrov@codix.bg>
2022-03-22 10:11:12 +0000

    Feature/prt 1645 bulk check sources

--- Modules/Sources/Http/Controllers/SourcesController.php
+++ Modules/Sources/Http/Controllers/SourcesController.php
@@ -4,6 +4,9 @@ namespace Modules\Sources\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Modules\Sources\Repositories\SourceRepository;
+use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
+use Modules\Sources\Models\Source;
 
 class SourcesController extends Controller
 {
@@ -16,5 +19,30 @@ class SourcesController extends Controller
     public function __construct(SourceRepository $repository)
     {
         $this->repository = $repository;
+    }
+
+    /**
+     * Search in sorces by file
+     *
+     * @param Request $request
+     * @return JsonResponse
+     */
+    public function search(Request $request) : JsonResponse
+    {
+        if (!$request->input('sources')) {
+            return new JsonResponse(["error" => "No valid sources list is provided."], 422);
+        }
+
+        try {
+            $sources = $request->input('sources');
+    
+            $result = Source::whereIn('source_name', $sources)
+                ->with(['department', 'departmentAssignedBy'])
+                ->get();
+
+            return new JsonResponse(["data" => $result->all()], 200);
+        } catch (\Exception $e) {
+            return new JsonResponse(['error' => $e], 400);
+        }
     }
 }
