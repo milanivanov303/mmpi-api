f39d095c2e9faf8598b27e489305de240c8e8eaa
slozanov <slozanov@codix.bg>
2023-09-19 18:38:33 +0300

    issue reassigment check person status

--- Modules/Issues/Http/Controllers/IssuesController.php
+++ Modules/Issues/Http/Controllers/IssuesController.php
@@ -211,7 +211,15 @@ class IssuesController extends Controller
         if ($branchModifications->count() > 0) {
             $branchModifications->groupBy('created_by_id')->each(function ($item) use ($tts_id, &$result) {
                 try {
-                    $newIssue = $this->createIssue($item[0]->createdBy->username, $item, $tts_id);
+                    $username = $item[0]->createdBy->username;
+                    if ($item[0]->createdBy->status != 1) { //check User if is not active
+                        $manager = User::where('id', $item[0]->createdBy->manager_id)->get()->toArray();
+                        $username = $manager[0]['username'];
+                        if ($manager[0]['status'] != 1) { //check PMO if is not active
+                            $username = 'cams_support';
+                        }
+                    }
+                    $newIssue = $this->createIssue($username, $item, $tts_id);
                     Log::channel('headmerege')->debug("
                             New issue {$newIssue->key} was created and assigned to user {$item[0]->createdBy->username}
                         ");
